71e454b17ef4baea09b1f1b95bba7e78f3f7c52a
pedroGoffi <pedrohgdepauloc@gmail.com>
2022-04-16 22:13:05 -0300

    Some minor change on gui

--- gui/src/Gui.java
+++ gui/src/Gui.java
@@ -3,43 +3,57 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Gui extends JFrame{
-  static class STD{
-    static JFrame frame;
-    static JMenuBar menuBar;
+  static JMenuBar  menuBar;
+  // STD FILE MENU
+  static JMenu     stdFileMenu;
+  // STD FILE MENU ITEM
+  static JMenuItem stdFileExit;
+  // STD EDIT MENU
+  static JMenu     stdEditMenu; 
+  // STD EDIT MENU ITEM
+  static JMenuItem stdEditProfile;
 
-    static int xFrameSize   = 500;
-    static int yFrameSize   = 500;
-    static String Title     = "My aplication";
-    public static void InitWindow(
-      JFrame newFrame,
-      JMenuBar newBar
-    ){
-      frame     = newFrame;
-      menuBar   = newBar;
-      frame.setSize(STD.xFrameSize, STD.yFrameSize);
-      frame.setTitle(STD.Title);
-
-
-      STD.addToMenuBar(new JMenu("File"));
-      STD.addToMenuBar(new JMenu("Edit"));
-      STD.addToMenuBar(new JMenu("Help"));
-
-      frame.add(menuBar);
-
-      frame.setVisible(true);
-    }
-    public static void addToMenuBar(JMenu newbar){
-      menuBar.add(newbar);
-    }
+  private static class STD{
+    static int xFrameSize = 500;
+    static int yFrameSize = 500;
+    static String imageIconPath = "../assets/icon.png";
   }
-  static class MENU{}
-  private void initVar(){
-    STD.InitWindow( 
-      new JFrame(), 
-      new JMenuBar() 
-    );
+  private static void HandleMenuBarAction(){
+    stdFileExit.addActionListener( e -> {
+      // If the exit button iss being clicked then 
+      // we will exit the program
+      System.out.println("Thanks for using me");
+      System.exit(0);
+    });
+    stdEditProfile.addActionListener( e ->{
+      System.out.println("Oh you clicked me >///<");
+    });
+  }
+  private static void HandleMenuBar(JFrame frame){
+    menuBar     = new JMenuBar();
+    // FILE MENU 
+    stdFileMenu = new JMenu("File");
+    stdFileExit = new JMenuItem("Exit");
+
+    stdFileMenu.add(stdFileExit);
+    // EDIT MENU
+    stdEditMenu     = new JMenu("Edit");
+    stdEditProfile  = new JMenuItem("Profile");
+
+    stdEditMenu.add(stdEditProfile);
+
+
+    menuBar.add(stdFileMenu);
+    menuBar.add(stdEditMenu);
+
+    frame.setJMenuBar(menuBar);
   }
   Gui(){
-    initVar();
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize( STD.xFrameSize, STD.yFrameSize );
+    HandleMenuBar(this);
+    HandleMenuBarAction();
+    this.setIconImage(new ImageIcon(STD.imageIconPath).getImage());
+    this.setVisible(true);
   }
 }
